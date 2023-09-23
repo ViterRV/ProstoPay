5a375e4f9ec6ee7d48ec2313b62875da6d0f8ff4
RV <viterrv@gmail.com>
2023-09-23 20:40:21 +0300

    Task2 add async

--- Task2/db.py
+++ Task2/db.py
@@ -1,17 +1,34 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, MetaData, Table, select
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-engine = create_engine('sqlite:///users.db', echo=True)
+database = "sqlite+aiosqlite:///users.db"
+engine = create_async_engine(database, echo=True)
+
+metadata = MetaData()
 
 class Base(DeclarativeBase):
     pass
 
 class User_DB(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key= True, autoincrement=True)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String(100))
     email = Column(String(100))
 
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(autoflush=False, bind=engine)
-session = Session()
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_name', String(100)),
+    Column('email', String(100))
+)
+
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+def get_async_session():
+    return async_session()
--- Task2/task2.py
+++ Task2/task2.py
@@ -1,8 +1,8 @@
+import asyncio
 from typing import Optional
-
 from pydantic import BaseModel
-from db import session, User_DB
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from db import get_async_session, users
 
 class UserDTO(BaseModel):
     id: Optional[int] = None
@@ -11,26 +11,33 @@ class UserDTO(BaseModel):
 
 class UserAction:
     def __init__(self):
-        self.session = session
+        self.async_db_session = get_async_session()
 
-    def get(self, user_id: int):
-        user = self.session.query(User_DB).filter_by(id=user_id).first()
-        if user:
-            return UserDTO(id=user.id, user_name=user.user_name, email=user.email)
-        else:
-            return None
+    async def get(self, user_id: int) -> Optional[UserDTO]:
+        async with self.async_db_session as async_db_session:
+            user = await async_db_session.execute(users.select().where(users.c.id == user_id))
+            user = user.fetchone()
+            if user:
+                return UserDTO(id=user.id, user_name=user.user_name, email=user.email)
+            else:
+                return None
 
-    def add(self, user: UserDTO):
-        new_user = User_DB(user_name=user.user_name, email=user.email)
-        self.session.add(new_user)
-        self.session.commit()
-        return UserDTO(id=new_user.id, user_name=new_user.user_name, email=new_user.email)
+    async def add(self, user: UserDTO) -> UserDTO:
+        async with self.async_db_session as async_db_session:
+            new_user = users.insert().values(user_name=user.user_name, email=user.email)
+            result = await async_db_session.execute(new_user)
+            await async_db_session.commit()
+            return UserDTO(id=result.inserted_primary_key[0], user_name=user.user_name, email=user.email)
 
+async def main():
+    user_action = UserAction()
 
-user_action = UserAction()
+    user_data = UserDTO(user_name="Ruslan", email="Ruslan@example.com")
+    added_user = await user_action.add(user_data)
+    print("Added User:", added_user)
 
-user_data = UserDTO(user_name="john_doe", email="john@example.com")
-added_user = user_action.add(user_data)
+    get_user = await user_action.get(added_user.id)
+    print("Retrieved User:", get_user)
 
-retrieved_user = user_action.get(added_user.id)
-print(retrieved_user)
+if __name__ == "__main__":
+    asyncio.run(main())
